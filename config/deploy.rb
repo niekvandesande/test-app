a83f1117447c73391616eeeb3701e44829415caa
Niek van de Sande <n.vandesande@bonvivantinsite.nl>
2017-03-16 16:55:36 +0100

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ server '86.83.154.136', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'https://github.com/niekvandesande/test-app.git'
 set :application,     'test-app'
-set :user,            'niek'
+set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -11,7 +11,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
