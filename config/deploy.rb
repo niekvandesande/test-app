3ecb95d662fd491d0fe0b1a5ce96294782b7f68d
Niek van de Sande <n.vandesande@bonvivantinsite.nl>
2017-03-22 22:45:16 +0100

    asdf

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :passenger_restart_with_touch, true
 
 ## Defaults:
 # set :scm,           :git
