397fcc3de142b42ed7df082d98c7d5253b2c2fe5
Niek van de Sande <n.vandesande@bonvivantinsite.nl>
2017-04-13 17:33:50 +0200

    added image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323134933) do
+ActiveRecord::Schema.define(version: 20170413152326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170323134933) do
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "tests", force: :cascade do |t|
